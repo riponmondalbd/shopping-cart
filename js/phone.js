9ab9d962f42536709a04d05a50a984c8381b1c00
Ripon Mondal <riponmondal149@gmail.com>
2023-08-24 00:32:28 +0600

    set phone plus and minus button working

--- js/phone.js
+++ js/phone.js
@@ -1,8 +1,34 @@
-document.getElementById('btn-phone-plus').addEventListener('click', function () {
+function updatePhoneNumber(isIncrease) {
     const phoneNumberField = document.getElementById('phone-number-field');
     const phoneNumberString = phoneNumberField.value;
     const previousPhoneNumber = parseInt(phoneNumberString);
 
-    const newPhoneNumber = previousPhoneNumber + 1;
+    let newPhoneNumber;
+    if (isIncrease) {
+        newPhoneNumber = previousPhoneNumber + 1;
+    }
+    else {
+        newPhoneNumber = previousPhoneNumber - 1;
+    }
     phoneNumberField.value = newPhoneNumber;
+
+    return newPhoneNumber;
+}
+
+function updatePhoneTotalPrice(newPhoneNumber) {
+    const phoneTotalPrice = newPhoneNumber * 1219;
+    const phoneTotalElement = document.getElementById('phone-total');
+    phoneTotalElement.innerText = phoneTotalPrice;
+}
+
+document.getElementById('btn-phone-plus').addEventListener('click', function () {
+    const newPhoneNumber = updatePhoneNumber(true);
+    updatePhoneTotalPrice(newPhoneNumber);
+
+
+});
+
+document.getElementById('btn-phone-minus').addEventListener('click', function () {
+    const newPhoneNumber = updatePhoneNumber(false);
+    updatePhoneTotalPrice(newPhoneNumber);
 })
---
+++ js/utilities.js
@@ -0,0 +1,16 @@
+function updateCaseNumber(isIncrease) {
+    const caseNumberField = document.getElementById('case-number-field');
+    const caseNumberString = caseNumberField.value;
+    const previousCaseNumber = parseInt(caseNumberString);
+
+    let newCaseNumber;
+    if (isIncrease === true) {
+        newCaseNumber = previousCaseNumber + 1;
+    }
+    else {
+        newCaseNumber = previousCaseNumber - 1;
+    }
+    caseNumberField.value = newCaseNumber;
+
+    return newCaseNumber;
+}
